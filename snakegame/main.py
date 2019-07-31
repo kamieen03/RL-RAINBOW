23602a548b215d5366291f67eae941892071ccfd
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2019-07-31 23:58:52 +0200

    Added main running the game

---
+++ snakegame/main.py
@@ -0,0 +1,11 @@
+import tkinter
+from snakegame.app import App
+
+def main():
+    root = tkinter.Tk()
+    app = App(root)
+    app.start()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
